1e6dd2e0544f52fac5f9dce24a018a8a3e339deb
Jeff Morrow <jwmorrow1s@gmail.com>
2025-03-05 21:31:06 -0600

    impl arena alloc/deinit

--- include/arena.h
+++ include/arena.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 /** START HEADER SECTION */
 
@@ -15,7 +16,7 @@ typedef struct ArenaModule ArenaModule;
 typedef struct Arena Arena;
 
 struct Arena {
-	uintptr_t* data; 
+	void* data; 
 	size_t cursor; 
 	size_t capacity; 
 	Arena* next;
@@ -36,6 +37,7 @@ void arena_module_init(ArenaModule *self);
 /** START IMPL SECTION */
 
 #ifdef ARENA_IMPLEMENTATION
+
 #define M_handle_alloc_failed(ptr) \
   do { \
 		if(!(ptr)){ \
@@ -43,24 +45,54 @@ void arena_module_init(ArenaModule *self);
 			exit(1);  \
 		} \
   } while(0)
+  
 
 static Arena* init_arena(size_t arena_size){
 	Arena* arena = malloc(sizeof(Arena));
 	M_handle_alloc_failed(arena);
+	memset(arena, 0, sizeof(Arena));
+
+	void* data = malloc(arena_size);
+	M_handle_alloc_failed(data);
+	memset(data, 0, arena_size);
+
 	arena->capacity = arena_size;
 	arena->cursor = 0;
+	arena->data = data;
+
 	return arena;
 }
 
 static void deinit_arena(Arena* self){
-	free(self->data);
-	free(self);
+	Arena* arena_iter = self;
+	while(arena_iter != NULL) {
+		Arena* arena_next = arena_iter->next;
+		free(arena_iter->data);
+		free(arena_iter);
+		arena_iter = arena_next;
+	}
 }
 
 static void* arena_alloc(Arena* self, size_t size){
-	self->data = malloc(size);
-	M_handle_alloc_failed(self->data);
-	return self->data;
+	if(size >= self->capacity) {
+		fprintf(stderr, "[FATAL] requested size for allocation greater than or equal to configured capacity.\n");
+		exit(1);
+	}
+	Arena* effective_arena = self;
+	while((effective_arena->cursor + size) >= effective_arena->capacity){ 
+		/** mark current arena as used up */
+		effective_arena->cursor = effective_arena->capacity;
+		/** allocate another arena */
+		Arena* next = init_arena(size);
+		/** assign new arena to next ptr */
+		effective_arena->next = next;
+		/** assign the iterator to the newly alloc'd arena */
+		effective_arena = effective_arena->next;
+	}
+	/** increment the cursor of the effective arena at this point */
+	effective_arena->cursor += size;
+	/** cast first to uint8_t to make the void* aligned to bytes, then cast back to void* for genericity */
+  return (void *)((uint8_t *)effective_arena->data + effective_arena->cursor);
 }
 
 void arena_module_init(ArenaModule *self){
